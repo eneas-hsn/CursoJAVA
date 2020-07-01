b86fc7fad913f6ed79946523b3150ac2fc7d5b28
eneas-hsn <eneashsn@gmail.com>
2020-06-30 22:15:06 -0300

    adicionando argumentos

--- src/helloworld.java
+++ src/helloworld.java
@@ -1,26 +1,9 @@
 public class helloworld {
 
     public static void main(String[] args) {
-        calc(3,3); //argumentos
+        System.out.println(args[0]+" aaaaaaaaaaaaaaaa "+args[1]);
 
-    }
-
-    static void calc(int x, int y) {
-//        static void > declaração padrão
-//        sum > nome da função
-//        () > define os parâmetros da função
-        System.out.println(x+y);
-        System.out.println(x/y);
-        System.out.println(x-y);
-        System.out.println(x*y);
     }
 }
-//   texto = string
-//   numero inteiro = int
-//   numero fracionado = double
-//   verdadeiro e falso = booleano
-//   ponto de entrada é o ponto onde todos os sistemas procumram no seu projeto para começar a iniciar o projeto.
-//   métodos são instruções que você define para o programa executar
-//   system.out.println() é uma instrução de chamada
 
 
