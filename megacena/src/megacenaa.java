e68f5e2f16e78d27d7585e136ef733d34b7173d1
eneas-hsn <eneashsn@gmail.com>
2020-07-01 22:06:19 -0300

    aprendendo loops

---
+++ megacena/src/megacenaa.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+
+public class megacenaa {
+    public static void main(String[] args) {
+        int x;
+        Random generate = new Random();
+//        int i=0;
+//        while(i<6){
+//            int number =generate.nextInt(60);
+//            System.out.println(number);
+//            i++;
+//        }
+        for (int i=0;i<6;i++){
+            int number=generate.nextInt(60);
+            System.out.println(number);
+        }
+    }
+
+}
