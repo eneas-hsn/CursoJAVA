6c132a6da0a1dba5227f51854787ea8cc5d2e6c9
eneas-hsn <eneashsn@gmail.com>
2020-07-04 19:16:42 -0300

    Update App.java

--- visibilidades/src/eneas/App.java
+++ visibilidades/src/eneas/App.java
@@ -7,4 +7,16 @@ public class App {
     public static double x=25;
 //    visibilidade private/ privada para todos os packages
     private static double z=15;
+
+    public static void main(String[] args) {
+//        tipos primitivos
+        String a="eae";  //não é tipo primitivo, e sim um objeto
+        int b=5; //inteiros
+        double c=5.5; //quebrados
+        byte d=127; //espaço de um byte
+        short e=12; //int só que com menos espaço
+        long f=11; //int só que com mais espaço
+        float g=12.1f; //double com menos espaço
+        char h='a'; //apenas um caractere
+    }
 }
