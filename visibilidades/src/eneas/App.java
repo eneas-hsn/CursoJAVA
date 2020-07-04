b1e4924843a8a8dab8df41f2c83451c1b6bcfb9f
eneas-hsn <eneashsn@gmail.com>
2020-07-04 19:36:15 -0300

    manipulação de strings

--- visibilidades/src/eneas/App.java
+++ visibilidades/src/eneas/App.java
@@ -9,21 +9,43 @@ public class App {
     private static double z=15;
 
     public static void main(String[] args) {
-//        tipos primitivos
-        String a="eae";  //não é tipo primitivo, e sim um objeto
-        int b=5; //inteiros
-        double c=5.5; //quebrados
-        byte d=127; //espaço de um byte
-        short e=12; //int só que com menos espaço
-        long f=11; //int só que com mais espaço
-        float g=12.1f; //double com menos espaço
-        char h='a'; //apenas um caractere
+        String x="  Hello world";
+        //quantidade de caracteres
+        System.out.println(x.length());
+        //concatenação
+        System.out.println(x+" e ae");
+        //ver se contem tal palavra
+        System.out.println(x.contains("hello"));
+        //ver onde começa tal palavra
+        System.out.println(x.indexOf("world"));
+        //capslock
+        System.out.println(x.toUpperCase());
+        //minusculo
+        System.out.println(x.toLowerCase());
+        //remove espaços desnecessarios
+        System.out.println(x.trim());
+        //ver texto a partir do numero selecionado
+        System.out.println(x.substring(4));
+        //da uma resposta booleana de acordo com uma comparação, vai ver se é igual
+        System.out.println(x.equals("eae"));
 
-//        conversão de tipo/casting
-//        é só colocar o tipo no inicio entre parenteses
-        double z=22.2;
-        int y=2;
-        float x=(float)z/y;
-        System.out.println(x);
+
+////        tipos primitivos
+//        String a="eae";  //não é tipo primitivo, e sim um objeto
+//        int b=5; //inteiros
+//        double c=5.5; //quebrados
+//        byte d=127; //espaço de um byte
+//        short e=12; //int só que com menos espaço
+//        long f=11; //int só que com mais espaço
+//        float g=12.1f; //double com menos espaço
+//        char h='a'; //apenas um caractere
+//
+////        conversão de tipo/casting
+////        é só colocar o tipo no inicio entre parenteses
+//        double z=22.2;
+//        int y=2;
+//        float x=(float)z/y;
+//        System.out.println(x);
+
     }
 }
